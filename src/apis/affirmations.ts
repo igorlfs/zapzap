273dc36a67e8c819d8a55759bfe0a3eb6d2bdd4a
Igor <igorlfs@ufmg.br>
2023-04-30 19:03:40 -0300

    chore: enable (code) spell checker

--- src/apis/affirmations.ts
+++ src/apis/affirmations.ts
@@ -1,13 +1,13 @@
 import WAWebJS from "whatsapp-web.js";
 import { send_request } from "./mod";
 
-type Affimation = {
+type Affirmation = {
     affirmation: string;
 };
 
 export async function send_affirmation(message: WAWebJS.Message) {
     send_request("https://www.affirmations.dev/").then(async (response) => {
-        const affirmation: Affimation = await response.json();
+        const affirmation: Affirmation = await response.json();
         message.reply(affirmation.affirmation);
     });
 }
